26cfa3562e466da6a3d0c5c353c788b8ff85e2ff
Shunkei <kakim002@umn.edu>
2023-10-06 12:51:57 -0500

    change readme

--- Recitation/rec5/build_PDFslides.R
+++ Recitation/rec5/build_PDFslides.R
@@ -1,7 +1,7 @@
 library(xaringanBuilder)
 library(renderthis)
 
-build_pdf("~/Dropbox/UM/TA/2_23_fall/PA8312_Discrimination/Critics/Presentation/PA8312_presentation.html")
+build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
 
 # xaringan::inf_mr(here("GitControlled/Presentation/Defense/presen_slides.rmd"))
 
