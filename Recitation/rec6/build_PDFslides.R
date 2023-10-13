f86bfddeeaacea5c76a077bfea03050674f92816
Shunkei <kakim002@umn.edu>
2023-10-13 13:28:19 -0500

    update rec6 slides

--- Recitation/rec6/build_PDFslides.R
+++ Recitation/rec6/build_PDFslides.R
@@ -1,7 +1,8 @@
 library(xaringanBuilder)
-# library(renderthis)
 
-build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
-
-# xaringan::inf_mr(here("GitControlled/Presentation/Defense/presen_slides.rmd"))
-
+# build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
+build_pdf(
+	"~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec6/recitation6_slides.html",
+	complex_slides = TRUE, 
+	partial_slides = TRUE
+	)
