512149993e504d3713324e2a2c19d9ed28053ae1
Shunkei3 <skakimoto3@huskers.unl.edu>
2023-10-13 19:08:02 -0500

    update rec6 slides

--- Recitation/rec6/build_PDFslides.R
+++ Recitation/rec6/build_PDFslides.R
@@ -1,5 +1,5 @@
+library(renderthis)
 library(xaringanBuilder)
-
 # build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
 build_pdf(
 	"~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec6/recitation6_slides.html",
