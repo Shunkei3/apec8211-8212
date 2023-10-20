123e937932eaf1cd7b2e15f82a3cb5338769133f
Shunkei3 <skakimoto3@huskers.unl.edu>
2023-10-20 01:20:36 -0500

    small changes

--- Recitation/rec3/build_PDFslides.R
+++
@@ -1,9 +0,0 @@
-library(xaringanBuilder)
-# library(renderthis)
-
-build_pdf(
-	"~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec3/recitation3_slides.html",
-	complex_slides = TRUE,
-	partial_slides = TRUE
-	)
-
--- Recitation/rec5/build_PDFslides.R
+++
@@ -1,9 +0,0 @@
-library(xaringanBuilder)
-
-# build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
-build_pdf(
-	"~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html",
-	complex_slides = TRUE, 
-	partial_slides = TRUE
-	)
-
